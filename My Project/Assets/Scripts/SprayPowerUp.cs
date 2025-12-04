9bbbbd974788a9723a2180fefa37f31f0e76c2c0
Igor Costa <igorcosta250705@gmail.com>
2025-12-04 11:34:42 -0300

    Spray PowerUp Added
    Agora temos um power up de spray que buffa seu dano!

--- My Project/Assets/Scripts/SprayPowerUp.cs
+++ My Project/Assets/Scripts/SprayPowerUp.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class SprayPowerUp : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Bullets e afins")]
+    [SerializeField] GameObject REDBIGBullet;
+    [SerializeField] GameObject GREENBIGBullet;
+    [SerializeField] GameObject redBullet;
+    [SerializeField] GameObject greenBullet;
+
+    [Header("Som de Pickup")]
+    public AudioClip PickupSFX;
+
+    private GunScript PlayerGun;
+
+    private Player PlayerRef;
+
     void Start()
     {
-        
+        gameObject.SetActive(true);
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
+        if(other.CompareTag("Player"))
+        {
+            PlayerRef = other.GetComponent<Player>();
+            //pegamos uma referencia do player
+
+            PlayerGun = PlayerRef.GetComponentInChildren<GunScript>();
+            // seu gunscript
+
+            PlayerGun.StartCoroutine("PowerUpEffect");
+
+            GlobalAudioPlayer.Instance.PlayPickupSound(PickupSFX);
+
+            gameObject.SetActive(false);
+        }
         
     }
 }
